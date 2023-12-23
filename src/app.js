a0dc9a13562ce1ffcff8e8182d4ed299f9781bdd
ibattistax <isabel.battista@gmail.com>
2023-12-23 10:00:05 -0500

    Added Search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+  let cityDisplayElement = document.querySelector("#city");
+  cityDisplayElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "oaff0b06238ce15e1bda3c4dt5f4a7ba";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+} //make an epi call and update interface
+
 function changeCitySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
   console.log(searchInput.value);
-  let cityDisplayElement = document.querySelector("#city");
-  cityDisplayElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", changeCitySearch);
+
+searchCity("London");
